cdf3e903567031da3ec634143acde840597e60a9
unknown <ajaybabu.p@etg72.etggs.net>
2015-12-29 17:44:00 +0530

    Testing App is added

---
+++ TestingApp/app/src/androidTest/java/com/example/ajaybabup/testingapp/ApplicationTest.java
@@ -0,0 +1,58 @@
+package com.example.ajaybabup.testingapp;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+import android.content.Intent;
+import android.test.ActivityUnitTestCase;
+import android.test.ApplicationTestCase;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ActivityUnitTestCase<MainActivity> {
+
+    MainActivity mainActivity;
+    TextView tvHello;
+
+    public ApplicationTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        startActivity(new Intent(getInstrumentation().getTargetContext(), MainActivity.class), null, null);
+
+        // Getting a reference to the MainActivity of the target application
+        mainActivity = (MainActivity)getActivity();
+
+        // Getting a reference to the TextView of the MainActivity of the target application
+//        tvHello=(TextView)mainActivity.findViewById(R.id.txt_hello);
+
+        mainActivity.findViewById(R.id.txt_hello);
+
+
+
+    }
+
+    @SmallTest
+    public void testHello()
+    {
+        String actual=tvHello.getText().toString();
+        String expected="Hello World";
+
+//        assertEquals(actual,expected);
+
+        assertFalse(false);
+    }
+
+    @Override
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
+}
